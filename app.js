3a9d5eeb00b3ab40a992f0135728fbee5e5bc278
Anant Tuli <mr.ananttuli@gmail.com>
2017-10-19 15:23:15 +1000

    fixed live - ES6 version

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ class BTCApp {
         this.totalPoints = totalPoints;
         this.data = [];
         this.dataset = [
-            { label: "USD/BTC", data: data, color: "#00FF00" }
+            { label: "USD/BTC", data: this.data, color: "#00FF00" }
         ];
         this.options = options;
         this.count = 0;
         this.currentMax = 0;
         this.currentMin = Number.MAX_VALUE;
         this.historicalData = [];
+        this.now = new Date();
 
     }
 
@@ -29,12 +30,11 @@ class BTCApp {
             }
 
         });
-        $(function () {
-            $("#timeDropdown").change(function () {
-                var text = $('#timeDropdown option:selected').val();
-                getHistoricalDataTime(text);
-            });
-        });
+       
+    }
+    updatePriceView(price){
+        $("#num").html('$<strong>' + price + '</strong>');
+
     }
     getDataFromAPI(currentTime) {
         let btcValues;
@@ -50,16 +50,17 @@ class BTCApp {
             error: function () {
                 //console.log('error');
             }
-        }).done(function () {
-            updatePriceView(btcValues['USD']['buy']);
+        }).done(()=>{
+            this.updatePriceView(btcValues['USD']['buy']);
             //updateNumber(btcValues['USD']['buy']);
-            pushToPlot([currentTime, btcValues['USD']['buy']]);
+            this.pushToPlot([currentTime, btcValues['USD']['buy']]);
         });
 
     }
     GetData() {
         let now = new Date().getTime();
-        getDataFromAPI(now += this.updateInterval);
+        this.now = now;
+        this.getDataFromAPI(this.now += this.updateInterval);
     }
     updatePlotRange(dataPoint){
         if (dataPoint[1] > this.currentMax) {
@@ -68,14 +69,14 @@ class BTCApp {
                         this.currentMax = this.options.yaxis.max;
                     } else if (dataPoint[1] < this.currentMin) {
                         this.options.yaxis.min = dataPoint[1] - 10;
-                        this.currentMin = options.yaxis.min;
+                        this.currentMin = this.options.yaxis.min;
                     }
     }
     pushToPlot(dataPoint) {
         this.data.push(dataPoint);
         this.count++;
-        updatePlotRange(dataPoint);
-        update();
+        this.updatePlotRange(dataPoint);
+        this.update();
         if (this.count > this.totalPoints) {
             this.data.shift();
 
@@ -84,24 +85,23 @@ class BTCApp {
 
         //setTimeout(getDataFromAPI(), updateInterval) executes the function and waits, then returns to setTimeout
         //Instead wait for time, then execute the anon function
-        setTimeout(function () { getDataFromAPI(now += updateInterval); }, this.updateInterval);
+        
 
-
+        setTimeout(()=>{
+            this.getDataFromAPI(this.now += this.updateInterval);
+        },this.updateInterval )
     }
 
     update() {
         $.plot($("#flot-placeholder1"), this.dataset, this.options);
     }
 
-    updatePriceView(price){
-        $("#num").html('$<strong>' + price + '</strong>');
 
-    }
     initApp() {
-        setLiveEventListeners();
-        drawHistoricalPlot();
+        this.setLiveEventListeners();
+        //drawHistoricalPlot();
         //getHistoricalData('1year');
-        GetData();
+        this.GetData();
     }
 }
 
@@ -179,157 +179,157 @@ function getInitOptions(){
     return options;
 }
 
-$(document).ready(function () {
-    getHistoricalData('1year');
-    $(function () {
-        $("#timeDropdown").change(function () {
-            var text = $('#timeDropdown option:selected').val();
-            getHistoricalDataTime(text);
-        });
-    });
+// $(document).ready(function () {
+//     getHistoricalData('1year');
+//     $(function () {
+//         $("#timeDropdown").change(function () {
+//             var text = $('#timeDropdown option:selected').val();
+//             getHistoricalDataTime(text);
+//         });
+//     });
 
-    var data = [];
-    var dataset;
-    var totalPoints = 50;
-    var updateInterval = 1000;
-    var now = new Date().getTime();
-    var count = 0;
-    var currentMax = 0;
-    var currentMin = 9999;
+//     var data = [];
+//     var dataset;
+//     var totalPoints = 50;
+//     var updateInterval = 1000;
+//     var now = new Date().getTime();
+//     var count = 0;
+//     var currentMax = 0;
+//     var currentMin = 9999;
 
-    var options = {
-        series: {
-            lines: {
-                show: true,
-                lineWidth: 1.2,
-                fill: false
-            }
-        },
-        xaxis: {
-            mode: "time",
-            tickSize: [2, "second"],
-            tickFormatter: function (v, axis) {
-                var date = new Date(v);
-                return "";
-            },
-            axisLabel: "Time",
-            axisLabelUseCanvas: true,
-            axisLabelFontSizePixels: 12,
-            // axisLabelFontFamily: 'Verdana, Arial',
-            axisLabelPadding: 10
-        },
-        yaxis: {
-            min: 0,
-            max: 0,
-            tickSize: 10,
-            // tickFormatter: function (v, axis) {
-            //     
-            // },
-            axisLabel: "USD/BTC",
-            axisLabelUseCanvas: true,
-            axisLabelFontSizePixels: 12,
-            // axisLabelFontFamily: 'Verdana, Arial',
-            axisLabelPadding: 6
-        },
-        legend: {
-            labelBoxBorderColor: "#fff"
-        },
-        grid: {
-            backgroundColor: "#000000",
-            //tickColor: "#008040",
-            hoverable: true,
-            clickable: true
-        }
-    };
+//     var options = {
+//         series: {
+//             lines: {
+//                 show: true,
+//                 lineWidth: 1.2,
+//                 fill: false
+//             }
+//         },
+//         xaxis: {
+//             mode: "time",
+//             tickSize: [2, "second"],
+//             tickFormatter: function (v, axis) {
+//                 var date = new Date(v);
+//                 return "";
+//             },
+//             axisLabel: "Time",
+//             axisLabelUseCanvas: true,
+//             axisLabelFontSizePixels: 12,
+//             // axisLabelFontFamily: 'Verdana, Arial',
+//             axisLabelPadding: 10
+//         },
+//         yaxis: {
+//             min: 0,
+//             max: 0,
+//             tickSize: 10,
+//             // tickFormatter: function (v, axis) {
+//             //     
+//             // },
+//             axisLabel: "USD/BTC",
+//             axisLabelUseCanvas: true,
+//             axisLabelFontSizePixels: 12,
+//             // axisLabelFontFamily: 'Verdana, Arial',
+//             axisLabelPadding: 6
+//         },
+//         legend: {
+//             labelBoxBorderColor: "#fff"
+//         },
+//         grid: {
+//             backgroundColor: "#000000",
+//             //tickColor: "#008040",
+//             hoverable: true,
+//             clickable: true
+//         }
+//     };
 
-    $("#flot-placeholder1").bind("plothover", function (event, pos, item) {
-        if (item) {
-            var x = getFormattedTime(item.datapoint[0]),
-                y = item.datapoint[1].toFixed(2);
-            $("#tooltipLive").html("Value at " + x + "<br><h3>$" + y + '</h3>')
+//     $("#flot-placeholder1").bind("plothover", function (event, pos, item) {
+//         if (item) {
+//             var x = getFormattedTime(item.datapoint[0]),
+//                 y = item.datapoint[1].toFixed(2);
+//             $("#tooltipLive").html("Value at " + x + "<br><h3>$" + y + '</h3>')
 
-                .fadeIn(500);
-        } else {
-            $("#tooltipLive").hide();
-        }
+//                 .fadeIn(500);
+//         } else {
+//             $("#tooltipLive").hide();
+//         }
 
-    });
-
-
-    function updateNumber(num) {
-
-        $("#num").html('$<strong>' + num + '</strong>');
-    }
-
-    function getDataFromAPI(currentTime) {
-        let btcValues;
-        $.ajax({
-            type: "GET",
-            url: "https://blockchain.info/ticker",
-            data: {},
-            success: function (response) {
-
-                btcValues = response;
-
-            },
-            error: function () {
-                //console.log('error');
-            }
-        }).done(function () {
-
-            updateNumber(btcValues['USD']['buy']);
-            pushToPlot([currentTime, btcValues['USD']['buy']]);
-        });
-
-    }
+//     });
 
 
+//     function updateNumber(num) {
+
+//         $("#num").html('$<strong>' + num + '</strong>');
+//     }
+
+//     function getDataFromAPI(currentTime) {
+//         let btcValues;
+//         $.ajax({
+//             type: "GET",
+//             url: "https://blockchain.info/ticker",
+//             data: {},
+//             success: function (response) {
+
+//                 btcValues = response;
+
+//             },
+//             error: function () {
+//                 //console.log('error');
+//             }
+//         }).done(function () {
+
+//             updateNumber(btcValues['USD']['buy']);
+//             pushToPlot([currentTime, btcValues['USD']['buy']]);
+//         });
+
+//     }
 
 
 
-    function pushToPlot(dataPoint) {
-        data.push(dataPoint);
-        count++;
-        if (dataPoint[1] > currentMax) {
-
-            options.yaxis.max = dataPoint[1] + 10;
-            currentMax = options.yaxis.max;
-        } else if (dataPoint[1] < currentMin) {
-            options.yaxis.min = dataPoint[1] - 10;
-            currentMin = options.yaxis.min;
-        }
 
 
-        update();
-        if (count > totalPoints) {
-            data.shift();
+//     function pushToPlot(dataPoint) {
+//         data.push(dataPoint);
+//         count++;
+//         if (dataPoint[1] > currentMax) {
 
-            count = 0;
-        }
-
-        //setTimeout(getDataFromAPI(), updateInterval) executes the function and waits, then returns to setTimeout
-        //Instead wait for time, then execute the anon function
-        setTimeout(function () { getDataFromAPI(now += updateInterval); }, updateInterval);
-
-
-    }
-    function GetData() {
-        data.shift();
-        getDataFromAPI(now += updateInterval);
-    }
+//             options.yaxis.max = dataPoint[1] + 10;
+//             currentMax = options.yaxis.max;
+//         } else if (dataPoint[1] < currentMin) {
+//             options.yaxis.min = dataPoint[1] - 10;
+//             currentMin = options.yaxis.min;
+//         }
 
 
-    GetData();
+//         update();
+//         if (count > totalPoints) {
+//             data.shift();
 
-    dataset = [
-        { label: "USD/BTC", data: data, color: "#00FF00" }
-    ];
+//             count = 0;
+//         }
 
-    $.plot($("#flot-placeholder1"), dataset, options);
-
-    function update() {
-        $.plot($("#flot-placeholder1"), dataset, options);
-    }
+//         //setTimeout(getDataFromAPI(), updateInterval) executes the function and waits, then returns to setTimeout
+//         //Instead wait for time, then execute the anon function
+//         setTimeout(function () { getDataFromAPI(now += updateInterval); }, updateInterval);
 
 
-});
+//     }
+//     function GetData() {
+//         data.shift();
+//         getDataFromAPI(now += updateInterval);
+//     }
+
+
+//     GetData();
+
+//     dataset = [
+//         { label: "USD/BTC", data: data, color: "#00FF00" }
+//     ];
+
+//     $.plot($("#flot-placeholder1"), dataset, options);
+
+//     function update() {
+//         $.plot($("#flot-placeholder1"), dataset, options);
+//     }
+
+
+// });
--- historical.js
+++ historical.js
@@ -1,171 +1,190 @@
 var historicalData = [];
 
-function  setupHistoricalEventListeners(){
-
-}
-function drawHistoricalPlot(){
-    getHistoricalDataTime('1year');
-    
+var drawHistoricalPlot = function () {
+    //-------CALLS--------
+    fetchHistoricalData('1year');
     setupHistoricalEventListeners();
-}
-function getHistoricalDataTime(time) {
-    let btcHistoricalValues;
-    var today = new Date();
-    var eDate, eYear, eMonth;
-    var sDate, sYear, sMonth;
 
-
-    eYear = today.getFullYear();
-    eMonth = today.getMonth() + 1;
-    eDate = today.getDate();
-
-    if (time == '1year') {
-        sMonth = eMonth;
-        sDate = eDate;
-        sYear = eYear - 1;
-    } else if (time == '6month') {
-        sMonth = (eMonth - 6) < 1 ? 12 - (eMonth - 6) : eMonth - 6;
-        sDate = eDate;
-        sYear = (eMonth - 6) < 1 ? eYear - 1 : eYear;
-    } else if (time == '3month') {
-        sMonth = (eMonth - 3) < 1 ? 12 - (eMonth - 3) : eMonth - 3;
-        sDate = eDate;
-        sYear = (eMonth - 3) < 1 ? eYear - 1 : eYear;
-    } else if (time == '1month') {
-        sMonth = (eMonth - 1) < 1 ? 12 - (eMonth - 1) : eMonth - 1;
-        sDate = eDate;
-        sYear = (eMonth - 1) < 1 ? eYear - 1 : eYear;
-    }
-
-
-    sMonth = (sMonth < 10) ? '0' + sMonth : sMonth;
-    eMonth = (eMonth < 10) ? '0' + eMonth : eMonth;
-
-    sDate = (sDate < 10) ? '0' + sDate : sDate;
-    eDate = (eDate < 10) ? '0' + eDate : eDate;
-
-
-
-    start = sYear;
-    end = eYear;
-    end += '-' + eMonth + '-' + eDate;
-    start += '-' + sMonth + '-' + sDate;
-
-    // url (required), options (optional)
-    //Date format yyyy-mm-day
-
-    //JavaScript fetch API
-    fetch('https://api.coindesk.com/v1/bpi/historical/close.json?start=' + start + '&end=' + end, {
-        method: 'get'
-    }).then(function (response) { return response.json(); })
-        .then(function (data) {
-            btcApp.historicalData
-            plotHistoricalData(data);
+    //----DEFINITIONS----------
+    function setupHistoricalEventListeners(){
+        $(function () {
+            $("#timeDropdown").change(function () {
+                var text = $('#timeDropdown option:selected').val();
+                fetchHistoricalData(text);
+                //getHistoricalDataTime(text);
+            });
         });
-
-}
-
-function calculateHistoricalLimits(data){
-    let max = 0, min = Number.MAX_VALUE;
-    for (let i = 0; i < data.length; i++) {
-
-        if (data[i][1] > max) {
-            max = data[i][1];
-        }
-        if (data[i][1] < min) {
-            min = data[i][1];
-        }
     }
-    return [min,max];    
-}
-function calculateHistoricalMaximum(data) {
-    let max = 0;
-    for (let i = 0; i < data.length; i++) {
+    function fetchHistoricalData(time) {
+        let btcHistoricalValues;
+        let start, end;
+        [start, end] = buildDateParams(time);
+        // url (required), options (optional)
+        //Date format yyyy-mm-day
+    
+        //JavaScript fetch API
+        fetch('https://api.coindesk.com/v1/bpi/historical/close.json?start=' + start + '&end=' + end, {
+            method: 'get'
+        }).then(function (response) { return response.json(); })
+            .then(function (data) {
+                
+                plotHistoricalData(data);
+            });
+    
+    }
 
-        if (data[i][1] > max) {
-            max = data[i][1];
+    function plotHistoricalData(data) {
+        historicalData = [];
+        storeResult(data);
+        
+    
+        let chartMinimum, chartMaximum;
+        [chartMinimum, chartMaximum] = calculateHistoricalLimits(historicalData);
+        var chartMin = calculateHistoricalMinimum(historicalData);
+        var chartMax = calculateHistoricalMaximum(historicalData);
+        $(function () {
+    
+            var dataset;
+            dataset = [
+                { label: "USD/BTC", data: historicalData, color: "#00FF00" }
+            ];
+    
+    
+    
+            $.plot("#historicalPlot", dataset, getInitHistoricalOptions());
+    
+    
+    
+            $("#historicalPlot").bind("plothover", function (event, pos, item) {
+                if (item) {
+                    var x = item.datapoint[0],
+                        y = item.datapoint[1].toFixed(2);
+                    x = new Date(x);
+                    var date = new Date(x);
+                    x = date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear();
+                    $("#historicalTooltip").html(item.series.label + " Value on " + x + "<br><h3>$" + y + '</h3>')
+    
+                        .fadeIn(500);
+                } else {
+                    $("#historicalTooltip").hide();
+                }
+    
+            });
+    
+        });
+    
+    }
+/**
+ * UTILITY FUNCTIONS
+ */
+
+    function buildDateParams(time) {
+        var today = new Date();
+        var eDate, eYear, eMonth;
+        var sDate, sYear, sMonth;
+
+
+        eYear = today.getFullYear();
+        eMonth = today.getMonth() + 1;
+        eDate = today.getDate();
+
+        if (time == '1year') {
+            sMonth = eMonth;
+            sDate = eDate;
+            sYear = eYear - 1;
+        } else if (time == '6month') {
+            sMonth = (eMonth - 6) < 1 ? 12 - (eMonth - 6) : eMonth - 6;
+            sDate = eDate;
+            sYear = (eMonth - 6) < 1 ? eYear - 1 : eYear;
+        } else if (time == '3month') {
+            sMonth = (eMonth - 3) < 1 ? 12 - (eMonth - 3) : eMonth - 3;
+            sDate = eDate;
+            sYear = (eMonth - 3) < 1 ? eYear - 1 : eYear;
+        } else if (time == '1month') {
+            sMonth = (eMonth - 1) < 1 ? 12 - (eMonth - 1) : eMonth - 1;
+            sDate = eDate;
+            sYear = (eMonth - 1) < 1 ? eYear - 1 : eYear;
         }
-    }
-    return max;
-}
 
-function calculateHistoricalMinimum(data) {
-    let min = Number.MAX_VALUE;
-    for (let i = 0; i < data.length; i++) {
-        if (data[i][1] < min) {
-            min = data[i][1];
+
+        sMonth = (sMonth < 10) ? '0' + sMonth : sMonth;
+        eMonth = (eMonth < 10) ? '0' + eMonth : eMonth;
+
+        sDate = (sDate < 10) ? '0' + sDate : sDate;
+        eDate = (eDate < 10) ? '0' + eDate : eDate;
+
+
+
+        start = sYear;
+        end = eYear;
+        end += '-' + eMonth + '-' + eDate;
+        start += '-' + sMonth + '-' + sDate;
+        eDate = today.getDate();
+
+        if (time == '1year') {
+            sMonth = eMonth;
+            sDate = eDate;
+            sYear = eYear - 1;
+        } else if (time == '6month') {
+            sMonth = (eMonth - 6) < 1 ? 12 - (eMonth - 6) : eMonth - 6;
+            sDate = eDate;
+            sYear = (eMonth - 6) < 1 ? eYear - 1 : eYear;
+        } else if (time == '3month') {
+            sMonth = (eMonth - 3) < 1 ? 12 - (eMonth - 3) : eMonth - 3;
+            sDate = eDate;
+            sYear = (eMonth - 3) < 1 ? eYear - 1 : eYear;
+        } else if (time == '1month') {
+            sMonth = (eMonth - 1) < 1 ? 12 - (eMonth - 1) : eMonth - 1;
+            sDate = eDate;
+            sYear = (eMonth - 1) < 1 ? eYear - 1 : eYear;
         }
+
+
+        sMonth = (sMonth < 10) ? '0' + sMonth : sMonth;
+        eMonth = (eMonth < 10) ? '0' + eMonth : eMonth;
+
+        sDate = (sDate < 10) ? '0' + sDate : sDate;
+        eDate = (eDate < 10) ? '0' + eDate : eDate;
+
+
+
+        start = sYear;
+        end = eYear;
+        end += '-' + eMonth + '-' + eDate;
+        start += '-' + sMonth + '-' + sDate;
+        return [start, end];
     }
-    return min;
-}
-function storeResult(data){
-    for (var k in data.bpi) {
-        var fldate = k.split('-');
-        var yr = parseInt(fldate[0]);
-        var mnth = parseInt(fldate[1]);
-        var dt = parseInt(fldate[2]);
 
-        var newDate = new Date(yr, mnth - 1, dt, 0, 0, 0, 0);
-
-        historicalData.push([newDate, data.bpi[k]]);
-    }
-}
-
-function getInitHistoricalOptions(){
-    let historicalOptions = {
-        series: {
-            lines: {
-                show: true,
-                fill: true
-            },
-            points: {
-                show: false
+    function calculateHistoricalLimits(data) {
+        let max = 0, min = Number.MAX_VALUE;
+        for (let i = 0; i < data.length; i++) {
+    
+            if (data[i][1] > max) {
+                max = data[i][1];
             }
-        },
-        grid: {
-            hoverable: true,
-            clickable: true
-        },
-        xaxis: {
-            mode: "time",
-            tickSize: [1, "day"],
-            tickColor: "#FFFFFF",
-            tickFormatter: function (v, axis) {
-                return "";
-            },
-            axisLabel: "Time",
-
-            axisLabelUseCanvas: true,
-            axisLabelFontSizePixels: 12,
-            // axisLabelFontFamily: 'Verdana, Arial',
-            //axisLabelPadding: 10
-        },
-        yaxis: {
-            min: chartMin + 30,
-            max: chartMax - 30,
-            axisLabelUseCanvas: true,
-            tickColor: "#FFFFFF"
+            if (data[i][1] < min) {
+                min = data[i][1];
+            }
         }
+        return [min, max];
+    }
 
-    };
+    function storeResult(data) {
+        for (var k in data.bpi) {
+            var fldate = k.split('-');
+            var yr = parseInt(fldate[0]);
+            var mnth = parseInt(fldate[1]);
+            var dt = parseInt(fldate[2]);
+    
+            var newDate = new Date(yr, mnth - 1, dt, 0, 0, 0, 0);
+    
+            historicalData.push([newDate, data.bpi[k]]);
+        }
+    }
+    
 
-    return historicalOptions;
-}
-function plotHistoricalData(data) {
-    storeResult(data);
-    historicalData = [];
-
-
-    var chartMin = calculateHistoricalMinimum(historicalData);
-    var chartMax = calculateHistoricalMaximum(historicalData);
-    $(function () {
-
-        var dataset;
-        dataset = [
-            { label: "USD/BTC", data: historicalData, color: "#00FF00" }
-        ];
-
-        var historicalOptions = {
+    function getInitHistoricalOptions() {
+        let historicalOptions = {
             series: {
                 lines: {
                     show: true,
@@ -187,7 +206,7 @@ function plotHistoricalData(data) {
                     return "";
                 },
                 axisLabel: "Time",
-
+    
                 axisLabelUseCanvas: true,
                 axisLabelFontSizePixels: 12,
                 // axisLabelFontFamily: 'Verdana, Arial',
@@ -199,39 +218,42 @@ function plotHistoricalData(data) {
                 axisLabelUseCanvas: true,
                 tickColor: "#FFFFFF"
             }
-
+    
         };
+    
+        return historicalOptions;
+    }
+};
 
-        $.plot("#historicalPlot", dataset, historicalOptions);
+//---------------
 
 
 
-        $("#historicalPlot").bind("plothover", function (event, pos, item) {
-            if (item) {
-                var x = item.datapoint[0],
-                    y = item.datapoint[1].toFixed(2);
-                x = new Date(x);
-                var date = new Date(x);
-                x = date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear();
-                $("#historicalTooltip").html(item.series.label + " Value on " + x + "<br><h3>$" + y + '</h3>')
 
-                    .fadeIn(500);
-            } else {
-                $("#historicalTooltip").hide();
-            }
 
-        });
 
-        $("#placeholder").bind("plotclick", function (event, pos, item) {
-            if (item) {
-                $("#clickdata").text(" - click point " + item.dataIndex + " in " + item.series.label);
-                plot.highlight(item.series, item.datapoint);
-            }
-        });
 
-        // Add the Flot version string to the footer
 
-        $("#footer").prepend("Flot " + $.plot.version + " &ndash; ");
-    });
 
-}
+
+// function calculateHistoricalMaximum(data) {
+//     let max = 0;
+//     for (let i = 0; i < data.length; i++) {
+
+//         if (data[i][1] > max) {
+//             max = data[i][1];
+//         }
+//     }
+//     return max;
+// }
+
+// function calculateHistoricalMinimum(data) {
+//     let min = Number.MAX_VALUE;
+//     for (let i = 0; i < data.length; i++) {
+//         if (data[i][1] < min) {
+//             min = data[i][1];
+//         }
+//     }
+//     return min;
+// }
+
